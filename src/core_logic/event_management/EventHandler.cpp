2a86759fdba70f341b7b1bdc3d28e73cd759617c
Caelean0 <152970304+Caelean0@users.noreply.github.com>
2024-06-09 16:04:16 +0200

    added Eventhandler Class

---
+++ src/core_logic/event_management/EventHandler.cpp
@@ -0,0 +1,5 @@
+//
+// Created by keanu on 6/9/2024.
+//
+
+#include "EventHandler.h"
---
+++ src/core_logic/event_management/EventHandler.h
@@ -0,0 +1,15 @@
+//
+// Created by keanu on 6/9/2024.
+//
+
+#ifndef HIVE_EVENTHANDLER_H
+#define HIVE_EVENTHANDLER_H
+
+
+class EventHandler
+{
+
+};
+
+
+#endif //HIVE_EVENTHANDLER_H
