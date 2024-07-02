313d42864e2e511df78be43937413eba1277a945
MrGrey3913 <151928222+MrGrey3913@users.noreply.github.com>
2024-07-03 00:58:48 +0200

    Tile Atlas Upgedated, Tutorial weitergebaut

---
+++ assets/data/hive_ARTI_TileAtlas_2024-07-02.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.2" name="hive_ARTI_TileAtlas_2024-07-02" tilewidth="24" tileheight="24" tilecount="65536" columns="256">
+ <image source="../graphics/hive_ARTI_TileAtlas_2024-07-02.png" width="6144" height="6144"/>
+</tileset>
