398cdcd648a281c627709c77913719b27c7de9ef
MrGrey3913 <151928222+MrGrey3913@users.noreply.github.com>
2024-07-03 16:27:59 +0200

    Update GameScene.cpp

--- src/scenes/GameScene.cpp
+++ src/scenes/GameScene.cpp
@@ -7,7 +7,7 @@
 
 Scenes::GameScene::GameScene(): Scene(std::make_shared<Camera2D>()),
                                 po_levels_(std::make_unique<std::vector<CoreLogic::DataProcessing::Level>>(std::initializer_list<CoreLogic::DataProcessing::Level>{
-                                        CoreLogic::DataProcessing::Level(std::make_unique<std::vector<std::string>>(std::initializer_list<std::string>{"assets/data/test.tmj", "lol"}), 0, CoreLogic::DataProcessing::LevelState::Default),
+                                        CoreLogic::DataProcessing::Level(std::make_unique<std::vector<std::string>>(std::initializer_list<std::string>{"assets/data/hive_DESI_Level0_2024-06-17.tmj", "lol"}), 0, CoreLogic::DataProcessing::LevelState::Default),
                                         CoreLogic::DataProcessing::Level(std::make_unique<std::vector<std::string>>(std::initializer_list<std::string>{"lel", "lol"}), 1, CoreLogic::DataProcessing::LevelState::War)
                         }))
 {
