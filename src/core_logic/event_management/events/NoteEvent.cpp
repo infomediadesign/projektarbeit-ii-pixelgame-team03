9d81e15c081b406621b88de06795ff57b923a7c7
Caelean0 <152970304+Caelean0@users.noreply.github.com>
2024-09-13 22:43:33 +0200

    noteevent

--- src/core_logic/event_management/events/NoteEvent.cpp
+++ src/core_logic/event_management/events/NoteEvent.cpp
@@ -5,7 +5,12 @@
 #include "NoteEvent.h"
 
 
-    namespace CoreLogic::EventManagement
+namespace CoreLogic::EventManagement
+{
+    NoteEvent::NoteEvent(std::shared_ptr<Object::Note> pa_note): InteractionEvent(NOTE)
     {
+        pa_note ->setVisible(true);
+        CoreLogic::DataProcessing::ActorStorage::setActiveNote(pa_note);
+        CoreLogic::DataProcessing::StateMachine::changeState()
     } // CoreLogic
 // EventManagement
