4bfee6a23265f7cc7fcb8f34258ae31a4bb90ae0
Caelean0 <152970304+Caelean0@users.noreply.github.com>
2024-09-14 03:55:54 +0200

    debug nice sachen

--- src/scenes/GameScene.cpp
+++ src/scenes/GameScene.cpp
@@ -36,7 +36,7 @@ Scenes::GameScene::GameScene(): Scene(std::make_shared<Camera2D>()),
     auto &eventHandler = CoreLogic::EventManagement::EventHandler::getInstance();
     eventHandler.resetPlayer();
 
-//    CoreLogic::DataProcessing::ActorStorage::unlockDrone(CoreLogic::EventManagement::Actors::Drone::DroneType::SCOUT);
+    CoreLogic::DataProcessing::ActorStorage::unlockDrone(CoreLogic::EventManagement::Actors::Drone::DroneType::SCOUT);
 }
 
 int Scenes::GameScene::getCurrentLevelID()
@@ -99,7 +99,13 @@ void Scenes::GameScene::update()
 
     CoreLogic::UserInterface::HUD& hud = *CoreLogic::UserInterface::HUD::getInstance();
     hud.update();
-    
+
+    if (IsKeyPressed(KEY_ZERO)) player->setElevation(0);
+    if (IsKeyPressed(KEY_ONE)) player->setElevation(1);
+    if (IsKeyPressed(KEY_TWO)) player->setElevation(2);
+
+    if (IsKeyPressed(KEY_NINE))
+        player->setElevation(9);
 
     if (IsKeyPressed(CoreLogic::DataProcessing::DesignConfig::PAUSE_KEYBOARD) || IsGamepadButtonPressed(CoreLogic::DataProcessing::DesignConfig::PAUSE_CONTROLLER, 0))
     {
